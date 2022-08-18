6822b2c419b70b35911b0e64c786eca297c19d24
anujsachdeva10 <anujsachdeva10>
2022-08-18 06:22:48 +0000

    from python anywhere

--- rapidrecruits/rapidrecruits/settings.py
+++ rapidrecruits/rapidrecruits/settings.py
@@ -134,3 +134,8 @@ LOGIN_URL = '/login/'
 
 TAGGIT_CASE_INSENSITIVE = True
 
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER  = 'rapidrecruits1.0@gmail.com'
+EMAIL_HOST_PASSWORD = 'kmlcmtzmkacqlmrh'
+EMAIL_USE_TLS = True
