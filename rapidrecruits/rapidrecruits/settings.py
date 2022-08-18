665e063e6a9eb51f3efc1bf34fffca992bd79bee
anujsachdeva10 <64650336+anujsachdeva10@users.noreply.github.com>
2022-08-18 14:53:27 +0530

    Update settings.py

--- rapidrecruits/rapidrecruits/settings.py
+++ rapidrecruits/rapidrecruits/settings.py
@@ -133,9 +133,3 @@ MEDIA_ROOT = MEDIA_DIR
 LOGIN_URL = '/login/'
 
 TAGGIT_CASE_INSENSITIVE = True
-
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER  = 'rapidrecruits1.0@gmail.com'
-EMAIL_HOST_PASSWORD = 'kmlcmtzmkacqlmrh'
-EMAIL_USE_TLS = True
