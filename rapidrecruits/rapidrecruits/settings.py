7754f5db204b0e3cc7e8a3213da8d635c530dd84
anujsachdeva10 <64650336+anujsachdeva10@users.noreply.github.com>
2022-08-21 00:34:38 +0530

    Update settings.py

--- rapidrecruits/rapidrecruits/settings.py
+++ rapidrecruits/rapidrecruits/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'joc@pv6v#^6*62qzch&e_9zyxx84e^*rz#wk&om_11b0q1qsxf'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["rapidrecruits.pythonanywhere.com"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -133,10 +133,3 @@ MEDIA_ROOT = MEDIA_DIR
 LOGIN_URL = '/login/'
 
 TAGGIT_CASE_INSENSITIVE = True
-
-
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'rapidrecruits1.0@gmail.com'
-EMAIL_HOST_PASSWORD = 'ifberbymldgdumte'
-EMAIL_USE_TLS = True
