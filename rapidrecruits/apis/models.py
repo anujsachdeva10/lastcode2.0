d41b2527e531407b257fbb57ab0c0c40636051ac
anujsachdeva10 <anujsachdeva10@gmail.com>
2022-08-22 01:27:53 +0530

    validations added

--- rapidrecruits/apis/models.py
+++ rapidrecruits/apis/models.py
@@ -4,6 +4,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 from taggit.managers import TaggableManager
+import datetime
 
 # Create your models here.
 
@@ -115,6 +116,7 @@ class VacancyApplicantMapping(models.Model):
     applicant = models.ForeignKey(User, on_delete = models.CASCADE, related_name = "applied_vacancies")
     vacancy = models.ForeignKey(VacanciesInfoModel, on_delete = models.CASCADE, related_name = "existing_applicants")
     status = models.CharField(max_length = 100, blank = True, null = True)
+    date_of_application = models.DateTimeField(default = datetime.datetime.now(), null = True, blank = True)
     def __str__(self):
         return self.applicant.username + " " + str(self.vacancy.id)
 
