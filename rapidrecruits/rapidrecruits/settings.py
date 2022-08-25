fa8c1c9dc91d70e98f5e871654f25be296fc513b
parth020806 <59834676+parth020806@users.noreply.github.com>
2022-08-25 11:10:39 +0530

    Update settings.py

--- rapidrecruits/rapidrecruits/settings.py
+++ rapidrecruits/rapidrecruits/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'joc@pv6v#^6*62qzch&e_9zyxx84e^*rz#wk&om_11b0q1qsxf'
 DEBUG = True
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['rapidrecruits.pythonanywhere.com']
 
 
 # Application definition
@@ -136,3 +136,8 @@ LOGIN_URL = '/login/'
 
 TAGGIT_CASE_INSENSITIVE = True
  
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'rapidrecruits1.0@gmail.com' 
+EMAIL_HOST_PASSWORD = 'ifberbymldgdumte'
+EMAIL_USE_TLS = True
